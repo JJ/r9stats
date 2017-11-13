1b2f04eaa8a804938811059c5e69495cc7831f8a
JJ Merelo <jjmerelo@gmail.com>
2017-11-13 09:50:02 +0100

    Añade info de usuarios

--- get-info.pl
+++ get-info.pl
@@ -6,7 +6,7 @@ use v5.14;
 
 use JSON;
 use LWP::UserAgent;
-use File::Slurper qw(read_lines);
+use File::Slurper qw(read_lines write_text);
 
 my $file = "IX_Jornadas_de_Usuarios_de_R.csv";
 
@@ -22,18 +22,30 @@ my @user_info;
 for my $m ( @members ) {
   my @parts = split(/\t/, $m);
   my ($id) = ($parts[1] =~ /(\d+)/);
+  if ( !$id ) {
+    ($id) = ($parts[8] =~ /(\d+)/);
+  }
   my $response = $ua->get("https://api.meetup.com/2/profile/$gg_gid/$id?&sign=true&key=$API_KEY");
-  if ( $response->is_success) {
+  if ( $response->is_success ) {
       my $user_json = $response->decoded_content;
       my $this_user = decode_json $user_json;
-      push @user_info, { lenguajes => $this_user->{'answers'}[0]{'answer'},
-			 os => $this_user->{'answers'}[1]{'answer'} };
+      push @user_info, {inscrito => $parts[6],
+			registrado => $parts[7],
+			lenguajes => $this_user->{'answers'}[0]{'answer'},
+			os => $this_user->{'answers'}[1]{'answer'} };
+      say $response->header("X-RateLimit-Remaining"), " to go ";
+      if ( $response->header("X-RateLimit-Remaining") <= 1 ) {
+	sleep($response->header("X-RateLimit-Reset"));
+	say "Sleeping";
+      }
   } else {
-      say $response->status_line;
-      say $response->
+    say "Error → $response->status_line";
+    say $response->header("X-RateLimit-Reset");
+    say $response->header("X-RateLimit-Limit");
+  }
 }
 
-write_text("r9-user-info.json",encode_json( @user_info ));
+write_text("r9-user-info.json", encode_json( \@user_info ));
 
 my %oss;
 my %lenguajes;
@@ -41,3 +53,5 @@ for my $u ( @user_info ) {
   $oss{'linux'}++ if $u->{'os'} =~ /inux/;
   $oss{'windows'}++ if $u->{'os'} =~ /indow/;
 }
+
+write_text("r9-user-oss.json", encode_json( \%oss ));
