9bdca0c9760ed6ed796c0e9edab275fe076cf725
JJ Merelo <jjmerelo@gmail.com>
2017-11-13 08:01:19 +0100

    Esperando hasta que me reseteen el límite

--- get-info.pl
+++ get-info.pl
@@ -17,10 +17,21 @@ my $API_KEY = $ENV{"MEETUP_API_KEY"};
 my @members = read_lines($file);
 
 shift @members;
+my @user_info;
 for my $m ( @members ) {
   my @parts = split(/\t/, $m);
   my ($id) = ($parts[1] =~ /(\d+)/);
   my $user_json = get "https://api.meetup.com/2/profile/$gg_gid/$id?&sign=true&key=$API_KEY";
-  
+  my $this_user = decode_json $user_json;
+  push @user_info, { lenguajes => $this_user->{'answers'}[0]{'answer'},
+		     os => $this_user->{'answers'}[1]{'answer'} };
 }
 
+write_text("r9-user-info.json",encode_json( @user_info ));
+
+my %oss;
+my %lenguajes;
+for my $u ( @user_info ) {
+  $oss{'linux'}++ if $u->{'os'} =~ /inux/;
+  $oss{'windows'}++ if $u->{'os'} =~ /indow/;
+}
