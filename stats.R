2271a9d93cab9eb1be399bd21ce52a7ca3b204a0
JJ Merelo <jjmerelo@gmail.com>
2017-11-15 18:23:12 +0100

    Una hora para esto... Odio R

--- stats.R
+++ stats.R
@@ -2,6 +2,43 @@ library(rjson)
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
+library(reshape2)
+
+
+all.data <- fromJSON(file="r9-user-info.json")
+data.df <- data.frame(os=character(),
+                      registrado=as.Date(character()),
+                      inscrito=as.Date(character()),
+                      lenguaje=character(),
+                      sexo=character())
+
+for ( i in 1:length(all.data) ) {
+    print(i)
+    print( all.data[[i]] )
+    if ( ! is.null(all.data[[i]]$os) )  {
+        os = all.data[[i]]$os
+    } else {
+        os = ""
+    }
+    if ( ! is.null(all.data[[i]]$lenguajes) )  {
+        lenguajes = all.data[[i]]$os
+    } else {
+        lenguajes = ""
+    }
+    if ( ! is.null(all.data[[i]]$sexo) )  {
+        sexo = all.data[[i]]$sexo
+    } else {
+        sexo = "X"
+    }
+    data.df <- rbind( data.df,
+                     data.frame(os=os,
+                                lenguajes=lenguajes,
+                                registrado=as.Date(all.data[[i]]$registrado,format="%m/%d/%Y"),
+                                inscrito=as.Date(all.data[[i]]$inscrito,format="%m/%d/%Y"),
+                                sexo=sexo )
+                     )
+}
+
 
 oss.data <- fromJSON(file="r9-user-oss.json")
 oss <- data.frame(os=names(oss.data),usuarios=c(oss.data$windows,oss.data$linux,oss.data$mac))
