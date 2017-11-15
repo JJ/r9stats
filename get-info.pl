b26b4da174e0c9e88d8a2f0183f407d619c44760
JJ Merelo <jjmerelo@gmail.com>
2017-11-15 17:39:36 +0100

    Datos con género ahora

--- get-info.pl
+++ get-info.pl
@@ -7,6 +7,7 @@ use v5.14;
 use JSON;
 use LWP::UserAgent;
 use File::Slurper qw(read_lines write_text);
+use Unicode::Normalize;
 
 my $file = "../../Descargas/IX_Jornadas_de_Usuarios_de_R(2).xls";
 
@@ -34,11 +35,14 @@ for my $m ( @members ) {
 
       # Encuentra el género
       my ($name,@cosas) = split( /\s+/, $parts[0] );
+      my $decomposed = NFKD( $name );
+      $decomposed =~ s/\p{NonspacingMark}//g;
+      $name = $decomposed;
       my $gender_response = $ua->get("https://api.genderize.io/?name=$name");
       my $gender;
       if ( $gender_response->is_success ) {
 	my $gender_json = decode_json $gender_response->decoded_content;
-	if ( $gender_json->{'probability'} > 0.55 ) {
+	if ( $gender_json->{'probability'} &&  $gender_json->{'probability'} > 0.55 ) {
 	  $gender = $gender_json->{'gender'};
 	} else {
 	  say "Can't say $name";
