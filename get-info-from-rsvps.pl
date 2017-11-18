744fed24db74ccc2ed42706a94e9c3dad6473cab
JJ Merelo <jjmerelo@gmail.com>
2017-11-18 08:53:39 +0100

    Obteniendo info completa

---
+++ get-info-from-rsvps.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.14;
+
+use JSON;
+use LWP::UserAgent;
+use File::Slurper qw(read_lines write_text);
+use Unicode::Normalize;
+
+my $file = "rsvps.json"; # Fichero privado que hay que descargarse desde la API
+my $rsvps_json = read_lines($file);
+my $rsvps = from_json $rsvps_json;
+
+my $gg_gid = 19214694;
+
+my $API_KEY = $ENV{"MEETUP_API_KEY"};
+my $ua = LWP::UserAgent->new;
+
+my @user_info;
+for my $m ( @$rsvps ) {
+  next if $m !~ /\d/;
+  say "→ $m";
+  my $response = $ua->get("https://api.meetup.com/2/profile/$gg_gid/$id?&sign=true&key=$API_KEY");
+  if ( $response->is_success ) {
+      my $user_json = $response->decoded_content;
+      my $this_user = decode_json $user_json;
+
+      # Encuentra el género
+      my ($name,@cosas) = split( /\s+/, $parts[0] );
+      my $decomposed = NFKD( $name );
+      $decomposed =~ s/\p{NonspacingMark}//g;
+      $name = $decomposed;
+      my $gender_response = $ua->get("https://api.genderize.io/?name=$name");
+      my $gender;
+      if ( $gender_response->is_success ) {
+	my $gender_json = decode_json $gender_response->decoded_content;
+	if ( $gender_json->{'probability'} &&  $gender_json->{'probability'} > 0.55 ) {
+	  $gender = $gender_json->{'gender'};
+	} else {
+	  say "Can't say $name";
+	  $gender = "X";
+	}
+      }
+      my $user = {inscrito => $parts[6],
+		  registrado => $parts[7],
+		  lenguajes => $this_user->{'answers'}[0]{'answer'},
+		  os => $this_user->{'answers'}[1]{'answer'},
+		  sexo => $gender };
+      
+      my $user_response = $ua->get("https://api.meetup.com/2/member/$id?&sign=true&key=$API_KEY");
+      
+      if ( $user_response->is_success ) {
+	my $profile_json = $user_response->decoded_content;
+	my $this_profile = decode_json $profile_json;
+	$user->{'lon'} = $this_profile->{'lon'};
+	$user->{'lat'} = $this_profile->{'lat'};
+	$user->{'city'} = $this_profile->{'city'};
+      }
+      
+      push @user_info, $user;
+      say $response->header("X-RateLimit-Remaining"), " to go ";
+      if ( $response->header("X-RateLimit-Remaining") <= 2 ) {
+	sleep($response->header("X-RateLimit-Reset"));
+	say "Sleeping";
+      }
+  } else {
+    say "Error → ", $response->status_line;
+    say $response->header("X-RateLimit-Reset");
+    say $response->header("X-RateLimit-Limit");
+  }
+}
+
+write_text("r9-user-info.json", to_json( \@user_info ));
+
+
